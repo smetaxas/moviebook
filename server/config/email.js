13b581708af470ed9ada776e01983d95b26bd3fd
Stylianos Metaxas <smetaxas03@hotmail.com>
2026-07-23 01:37:55 +0300

    Add email OTP login

--- server/config/email.js
+++ server/config/email.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 25,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
