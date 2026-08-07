17c1ee11cc0b4346d0358bea25b203b0616bf805
Stylianos Metaxas <smetaxas03@hotmail.com>
2026-08-07 18:15:54 +0300

    Add JWT expiry auto-logout

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,8 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 })
 
-console.log('API URL:', import.meta.env.VITE_API_URL)
-
+// Request interceptor - add token
 api.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem('user'))
   if (user?.token) {
@@ -13,5 +12,17 @@ api.interceptors.request.use((config) => {
   }
   return config
 })
+
+// Response interceptor - handle token expiry
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('user')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default api
